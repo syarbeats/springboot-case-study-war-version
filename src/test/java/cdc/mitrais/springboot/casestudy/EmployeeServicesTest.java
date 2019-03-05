20765273a68c6fe8037b2c30f5fcd1f5873b2110
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-03-05 12:32:48 +0700

    add unit testing for service layer

---
+++ src/test/java/cdc/mitrais/springboot/casestudy/EmployeeServicesTest.java
@@ -0,0 +1,67 @@
+package cdc.mitrais.springboot.casestudy;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import cdc.mitrais.springboot.casestudy.config.EmployeeUserDetailsService;
+import cdc.mitrais.springboot.casestudy.dao.EmployeeRepository;
+import cdc.mitrais.springboot.casestudy.model.Employee;
+import cdc.mitrais.springboot.casestudy.services.EmployeeServices;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeServicesTest {
+
+	@Autowired
+	private EmployeeUserDetailsService employeeUserDetailsService;
+	@Autowired
+	private EmployeeServices employeeService;
+	@Autowired
+	private EmployeeRepository empRepo;
+	
+	private UserDetails userDetails;
+	private Authentication authToken;
+	
+	@Before
+	public void setUp() throws Exception {
+		userDetails = employeeUserDetailsService.loadUserByUsername ("admin");
+        authToken = new UsernamePasswordAuthenticationToken (userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+	}
+
+	@Test
+	@Transactional
+    @Rollback(true)
+	public void testAddEmployee() {
+		
+		employeeService.addEmployee(new Employee(909, "Elthon John", 75000));
+		Assert.assertEquals("Elthon John", employeeService.getEmployeeById(909).getName());
+	}
+	
+	@Test
+	@Transactional
+    @Rollback(true)
+	public void testEditEmployee() {
+		/*
+		 * Employee with id=406, name=Elvis Presley, salary=55000
+		 * **/
+		Employee emp = employeeService.getEmployeeById(406); 
+		emp.setSalary(60000);/*Update his salary to 60000*/
+		employeeService.updateEmployeeSalary(emp);
+		Assert.assertEquals(60000, employeeService.getEmployeeById(406).getSalary());
+	}
+
+}
