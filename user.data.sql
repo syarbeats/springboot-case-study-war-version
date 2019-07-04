12df9ca98cc5f1f7021885324035094837d26ec9
Syarif Hidayat <srf.hidayat@gmail.com>
2019-07-04 15:14:16 +0700

    Update user.data.sql

--- user.data.sql
+++ user.data.sql
@@ -5,4 +5,4 @@ using the following syntax:
 String encoded=new BCryptPasswordEncoder().encode("user123");
 String adminPassword=new BCryptPasswordEncoder().encode("admin123");
 
-insert into user(id, username, password, enabled) values(2, 'user', '$2a$10$fV.0oNiKTmVja3.VoyLrg.XpHZJUzEwfjlQruq//rouovq7MJ3Dbe', true); 
+insert into user(id, username, password, enabled, role) values(2, 'user', '$2a$10$fV.0oNiKTmVja3.VoyLrg.XpHZJUzEwfjlQruq//rouovq7MJ3Dbe', true, 'ROLE_ADMIN'); 
