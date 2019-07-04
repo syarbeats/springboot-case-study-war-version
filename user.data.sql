f8e9d0c3adffde2af43bea0f45c174845c345173
Syarif Hidayat <srf.hidayat@gmail.com>
2019-07-04 15:16:54 +0700

    Update user.data.sql

--- user.data.sql
+++ user.data.sql
@@ -2,7 +2,7 @@
 please replace password field with your Bcrypt encoding result for user123
 using the following syntax:
 
-String encoded=new BCryptPasswordEncoder().encode("user123");
+String encoded=new BCryptPasswordEncoder().encode("user");
 String adminPassword=new BCryptPasswordEncoder().encode("admin123");
 
-insert into user(id, username, password, enabled, role) values(2, 'user', '$2a$10$fV.0oNiKTmVja3.VoyLrg.XpHZJUzEwfjlQruq//rouovq7MJ3Dbe', true, 'ROLE_ADMIN'); 
+insert into user(id, username, password, enabled, role) values(1, 'admin', '$2a$10$fV.0oNiKTmVja3.VoyLrg.XpHZJUzEwfjlQruq//rouovq7MJ3Dbe', true, 'ROLE_ADMIN'); 
